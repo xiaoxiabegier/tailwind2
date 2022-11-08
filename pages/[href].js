6b60cb376ce13a3cfd9cbff7f4568e274cacec39
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-08 11:11:32 -0800

    intial

--- pages/[href].js
+++ pages/[href].js
@@ -42,9 +42,48 @@ export default function Page(props){
             } else  links.push({label: "", href:"/"})
         }
 
-const appearance = {
-  theme: 'stripe',
-  }
+        const appearance = {
+            theme: 'flat',
+            variables: {
+              fontFamily: ' "Gill Sans", sans-serif',
+              fontLineHeight: '1.5',
+              borderRadius: '10px',
+              colorBackground: '#F6F8FA',
+              colorPrimaryText: '#262626'
+            },
+            rules: {
+              '.Block': {
+                backgroundColor: 'var(--colorBackground)',
+                boxShadow: 'none',
+                padding: '12px'
+              },
+              '.Input': {
+                padding: '12px'
+              },
+              '.Input:disabled, .Input--invalid:disabled': {
+                color: 'lightgray'
+              },
+              '.Tab': {
+                padding: '10px 12px 8px 12px',
+                border: 'none'
+              },
+              '.Tab:hover': {
+                border: 'none',
+                boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 7px rgba(18, 42, 66, 0.04)'
+              },
+              '.Tab--selected, .Tab--selected:focus, .Tab--selected:hover': {
+                border: 'none',
+                backgroundColor: '#fff',
+                boxShadow: '0 0 0 1.5px var(--colorPrimaryText), 0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 7px rgba(18, 42, 66, 0.04)'
+              },
+              '.Label': {
+                fontWeight: '500'
+              }
+            }
+          };
+          
+          // Pass the appearance object to the Elements instance
+        //   const elements = stripe.elements({clientSecret, appearance});
 
     const options = {
         clientSecret,
